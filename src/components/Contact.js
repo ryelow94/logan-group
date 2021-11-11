a188c48972333c725932c4d7fa3243b127c7d145
Ryan Logan <ryan_plogan@yahoo.com>
2021-11-10 20:59:37 -0800

    added more validation to contact form, still needs some work

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,22 +7,21 @@ import { useRef } from "react";
 const validate = (values) => {
   const errors = {};
   if (!values.subject) {
-    errors.subject = "Subject is a required field";
+    errors.subject = "Required field";
   }
   if (!values.name) {
-    errors.name = " Name is a required field";
+    errors.name = " Required field";
   }
   if (!values.email) {
-    errors.email = "Email is a required field";
+    errors.email = "Required field";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = "Invalid email address";
   }
   if (!values.message) {
-    errors.message = "Message is a required field";
+    errors.message = "Required field";
   }
   return errors;
 };
-
 function ContactForm() {
   const form = useRef();
   const formik = useFormik({
@@ -34,8 +33,6 @@ function ContactForm() {
     },
     validate,
     onSubmit: (values) => {
-      let valueString = JSON.stringify(values);
-      console.log(valueString);
       emailjs
         .sendForm(
           "service_666aw17",
@@ -52,13 +49,15 @@ function ContactForm() {
           }
         );
         formik.resetForm();
+        alert("Thank you, your message has been sent")
     },
   });
   return (
     <div className="container">
       <form ref={form} onSubmit={formik.handleSubmit}>
         <div>
-          <label htmlFor="subject">Subject *</label>
+        {formik.errors.subject ? <div className="required-field">{formik.errors.subject}</div> : null}
+          <label htmlFor="subject">Subject </label>
           <select name="subject" id="subject" value={formik.values.subject} onChange={formik.handleChange}>
             <option value="">--Please choose an option--</option>
             <option value="Schedule an appointment">Schedule an Appointment</option>
@@ -67,14 +66,19 @@ function ContactForm() {
             <option value="info">Request Info</option>
             <option value="general">General</option>
           </select>
-          <label>Name *</label>
+          {formik.errors.name ? <div className="required-field">{formik.errors.name}</div> : null}
+          <label>Name </label>
           <input type="text" id="name" name="name" value={formik.values.name} onChange={formik.handleChange} />
-          <label>Email *</label>
+          {formik.errors.email ? <div className="required-field"> {formik.errors.email}</div> : null}
+          <label>Email </label>
           <input type="text" id="email" name="email" value={formik.values.email} onChange={formik.handleChange}/>
           <label>Phone</label>
           <input type="text" id="phone" name="phone" value={formik.values.phone} onChange={formik.handleChange}/>
-          <label>Message *</label>
+          {formik.errors.message ? <div className="required-field">{formik.errors.message}</div> : null}
+          <label>Message </label>
           <input type="text" id="message" name="message" value={formik.values.message} onChange={formik.handleChange} />
+          {formik.errors ? <div className="required-field"> All required fields must be entered before submitting</div> : null}
+          <br></br>
           <input type="submit" />
         </div>
       </form>
