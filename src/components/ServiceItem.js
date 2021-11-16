457e5ba849853fb2f93847595a1e1f1865d57967
Ryan Logan <ryan_plogan@yahoo.com>
2021-11-15 17:11:54 -0800

    services component added, but css still needs some fixing for description

--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -1,26 +1,26 @@
 export const ServiceItems = [
     {
         title: "Retirement Planning",
-        description:"",
+        description1:" Here is where a description will go",
         tsafaq: "403(b)/TSA FAQ",
         cName: "service-item",
         id:"retire"
     },
     {
         title:"CalSTRS/PRS Pension Planning",
-        description:"",
+        description2:"Here is where a description will go. a;dsjfkasdfajsdf;kasjdfkla;sjdf;kasdjf;klasdjf;kasdfjkl;asdjfkl;asdjf;kasdjfk;asdjf;alksdfj;askdfj;asdklfj;asdlfkjasdf;klasdfj;asdfk;ajsdf;laksdfjl;asdfk;jasdf;alsdfjla;sdfkla;sdf;aklsdjkl;asd;lkasdlkjasdf;jasd;lkjasd;asdfjlkasdfl;ksadfjsdf;sadjkasdffjkl;fsd;sdklsdfklsdklsdlkdsfkjlsdjalkljdsfkjlsdfklsadfkljsdakjlskdakljfsdklsaklj;sdfkjlsdklajdskklsdfkjldfs;kjlfdsakjl;sdakljfsdjkl",
         cName:"service-item",
         id:"pension"
     },
     {
         title:"College Savings Plans (529)",
-        description:"",
+        description3:"here is where a description will go",
         cName:"service-item",
         id:"college"
     },
     {
         title:"Estate Planning",
-        description:"",
+        description4:"here is where a description will go",
         cName:"service-item",
         id:"estate"
     }
--- src/components/Services.js
+++ src/components/Services.js
@@ -2,42 +2,81 @@ import React from "react";
 import { ServiceItems } from "./ServiceItem";
 import "./Services.css";
 import { Faq } from "./Tsafaq";
-import { useState} from "react"
+import { useState } from "react";
 
 const Services = () => {
-  const [faqClicked, setFaqClicked]= useState(false)
-  const [showFaq, setShowFaq]= useState(false)
-  const handleClick = () => {
-    if(faqClicked===false){
-      setFaqClicked(true)
-    } else if(faqClicked===true){
-      setFaqClicked(false)
+  const [faqClicked, setFaqClicked] = useState(false);
+  const [showFaq, setShowFaq] = useState(false);
+  const [showDescripion1, setShowDescripion1] = useState(false);
+  const [showDescripion2, setShowDescripion2] = useState(false);
+  const [showDescripion3, setShowDescripion3] = useState(false);
+  const [showDescripion4, setShowDescripion4] = useState(false);
+  const handleFaq = () => {
+    if (faqClicked === false) {
+      setFaqClicked(true);
+    } else if (faqClicked === true) {
+      setFaqClicked(false);
     }
   };
   const handleServiceClick = (e) => {
-    console.log(e.target.id)
-    if(e.target.id === "retire"){
-      setShowFaq(true)
-    } 
-    if(e.target.id === "retire" && showFaq===true){
-      setShowFaq(false)
+    console.log(e.target);
+    if (e.target.id === "retire") {
+      setShowFaq(true);
+      setShowDescripion1(true);
     }
-
-  }
-    return (
-      <div>
-        <h1 id="services-header">Services</h1>
-        <div className="card">
-          {ServiceItems.map((item, index, id) => {
-            return (
-              <div key={index} className={item.cName}>
-                <h4 id={item.id} onClick={(e)=> handleServiceClick(e)}> {item.title}</h4>
-                <h5>{item.description}</h5>
+    if (e.target.id === "retire" && showFaq === true) {
+      setShowFaq(false);
+    }
+    if (e.target.id === "retire" && showDescripion1 === true) {
+      setShowDescripion1(false);
+    }
+    if (e.target.id === "pension") {
+      setShowDescripion2(true);
+    }
+    if (e.target.id === "pension" && showDescripion2 === true) {
+      setShowDescripion2(false);
+    }
+    if (e.target.id === "college") {
+      setShowDescripion3(true);
+    }
+    if (e.target.id === "college" && showDescripion3 === true) {
+      setShowDescripion3(false);
+    }
+    if (e.target.id === "estate") {
+      setShowDescripion4(true);
+    }
+    if (e.target.id === "estate" && showDescripion4 === true) {
+      setShowDescripion4(false);
+    }
+  };
+  return (
+    <div>
+      <h1 id="services-header">Services</h1>
+      <div className="cards">
+        {ServiceItems.map((item, index) => {
+          return (
+            <div key={index} className={item.cName}>
+              <h4 id={item.id} onClick={(e) => handleServiceClick(e)}>
+                {item.title}
+              </h4>
+              <div className="description-div">
+                {showDescripion1 ?<h5 className="descripion"> {item.description1}</h5> : null}
+                {showDescripion2 ?<h5 className="descripion"> {item.description2}</h5>: null}
+                {showDescripion3 ?<h5 className="descripion"> {item.description3}</h5> : null}
+                {showDescripion4 ?<h5 className="descripion"> {item.description4}</h5>: null}
+                </div>
                 {item.tsafaq ? (
                   <div>
-                    <button style={{display:showFaq?"block": "none"}} className="faq-button" onClick={handleClick}>{item.tsafaq}</button>
+                    <div className="button-div">
+                      <button
+                        style={{ display: showFaq ? "block" : "none" }}
+                        className="faq-button"
+                        onClick={handleFaq}
+                      >
+                        {item.tsafaq}
+                      </button>
+                      </div>
                     {faqClicked ? (
-                      <div>
                         <h5>
                           {Faq.map((item, index) => {
                             return (
@@ -50,16 +89,15 @@ const Services = () => {
                             );
                           })}
                         </h5>
-                      </div>
                     ) : null}
                   </div>
                 ) : null}
-              </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Services;
