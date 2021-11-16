48d883ec74a74d2994211c64de5a0aa0dcb0c3fc
McKendree Strommer <mckendree.strommer@gmail.com>
2021-11-16 11:45:59 -0800

    added an inline calendar to the calendar route, currently connects with my calendly account

---
+++ src/components/Calendar.js
@@ -0,0 +1,11 @@
+import { InlineWidget } from "react-calendly";
+
+const Calendar = () => {
+  return (
+    <>
+      <InlineWidget url="https://calendly.com/mckendree-strommer" />
+    </>
+  );
+};
+
+export default Calendar;
