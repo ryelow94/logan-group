f3c75001cca7e444c709fe815d3fad6124a648e1
Logan <ryanx.logan@intel.com>
2021-11-23 16:49:49 -0800

    started carousel for about

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { aboutItems } from "./About-items";
-import "./About.css"
+import "./About.css";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const About = () => {
-
   return (
     <div>
       <h1>About Us</h1>
+        {aboutItems.map((item,i) => {
+          return (
+            <CarouselProvider
+        naturalSlideWidth={20}
+        naturalSlideHeight={20}
+        totalSlides={3}
+      >
+            <Slider>
+          <Slide index={item[i]}>{item.aboutDesc}.</Slide>
+          <Slide >I am the second Slide.</Slide>
+          <Slide >I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+        </CarouselProvider>
+          )
+        })}
     </div>
   );
 };
