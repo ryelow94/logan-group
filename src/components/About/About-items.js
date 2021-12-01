0d5ea3d1c32770c0aaa7aba6669bcb1cd68c43a5
Logan <ryanx.logan@intel.com>
2021-12-01 14:50:23 -0800

    added photos for about

--- src/components/About/About-items.js
+++ src/components/About/About-items.js
@@ -1,6 +1,6 @@
 export const aboutItems = [
     {
-        aboutImg: "https://www.akc.org/wp-content/uploads/2018/01/cute-puppy-body-image.jpg",
+        aboutImg: "https://i.dailymail.co.uk/i/pix/scaled/2014/10/23/1414056186682_wps_17_Pic_By_HotSpot_Media_CHEE.jpg",
         aboutName: "Kevin Logan",
         aboutTitle: "Founder/CEO",
         aboutDesc: "Hello",
@@ -13,7 +13,7 @@ export const aboutItems = [
         aboutId: "Kevin"
     },
     {
-        aboutImg: "https://www.akc.org/wp-content/uploads/2018/01/cute-puppy-body-image.jpg",
+        aboutImg: "https://api.time.com/wp-content/uploads/2014/09/macaca_nigra_self-portrait_rotated_and_cropped.jpg",
         aboutName:"Grant Logan",
         aboutTitle: "Operations Manager",
         aboutDesc: "Hello again",
@@ -26,7 +26,7 @@ export const aboutItems = [
         aboutId: "Grant"
     },
     {
-        aboutImg: "https://www.akc.org/wp-content/uploads/2018/01/cute-puppy-body-image.jpg",
+        aboutImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPuaXCGM6j0prMgZ41hMkH0hAnKA_6Qzq06w&usqp=CAU",
         aboutName:"Joseph Palatnik",
         aboutTitle: "Administrative Assistant",
         aboutDesc: "Hello again again",
