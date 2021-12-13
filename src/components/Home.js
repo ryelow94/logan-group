bbdfd86c91cfeb5acde37354966ce8c39afa87a4
Logan <ryanx.logan@intel.com>
2021-12-13 15:39:27 -0800

    added a photo and a map for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 const Home = () => {
     return(
       <div>
+        <img src="https://images.crexi.com/assets/561082/9856a247400c45ef9c85eeb95454b519_716x444.jpg"/>
         <p>
           The Logan Group Securities is a registered Broker/Dealer specializing
           in retirement planning for Educators. We offer a wide variety of
@@ -22,7 +23,8 @@ const Home = () => {
           The Logan Group Securities believes that client services should be regarded
           above all other priorities in our business. That's why we are
           committed to serving our clients on a long-term basis.
-        </p>
+        </p> 
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3111.871299754913!2d-121.24964818479567!3d38.743715479594705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809b1fef63094151%3A0x4c3b7399352e8902!2s2150%20Douglas%20Blvd%2C%20Roseville%2C%20CA%2095661!5e0!3m2!1sen!2sus!4v1639437167481!5m2!1sen!2sus"></iframe>
       </div>
     );
   }
